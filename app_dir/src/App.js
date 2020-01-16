4d3a118717580974d532e309140225548dbfafd5
Olivier Heimerdinger <olivier@heimerdinger.me>
2020-01-16 12:51:19 +0100

    Udpate App.js
    Remove my name

--- app_dir/src/App.js
+++ app_dir/src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div>
       <h1 style={{textAlign: "center", marginTop:"2rem"}}>React Application with Electron</h1>
       <p className="cl_ho_horloge" style={{ textAlign: "center", marginTop: "1rem" }}></p>
-      <p>Olivier Heimerdinger</p>
     </div>
   );
 }
