fb5ef3c5cda50d0c563521712ba3b5f4bb531eb6
Olivier Heimerdinger <olivier@heimerdinger.me>
2020-01-16 17:38:37 +0100

    Update App.js
    Add communication between react and electron

--- app_dir/src/App.js
+++ app_dir/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1 style={{textAlign: "center", marginTop:"2rem"}}>React Application with Electron</h1>
-      <p className="cl_ho_horloge" style={{ textAlign: "center", marginTop: "1rem" }}></p>
-    </div>
-  );
+const { ipcRenderer } = window.require("electron")
+console.log("ERROR, SEE: https://github.com/electron/electron/issues/9920");
+
+class App extends React.Component {
+  handleBtnClick = (e) => {
+    ipcRenderer.send("state", "connected")
+  }
+  render(){
+      return (
+        <div>
+          <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
+            React Application with Electron
+          </h1>
+          <p
+            className="cl_ho_horloge"
+            style={{ textAlign: "center", marginTop: "1rem" }}
+          ></p>
+          <p style={{ textAlign: "center", marginTop: "2rem" }}>
+            <div className="btn btn-outline-dark" onClick={ this.handleBtnClick }>
+              Ajouter le menu
+            </div>
+          </p>
+        </div>
+      );
+  }
+
 }
 
 export default App;
